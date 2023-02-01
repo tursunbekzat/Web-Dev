ffbac61ce1a5a52cb507253da98e5ae22d6ead35
Bekzat <b_tursyn@kbtu.kz>
2023-02-01 21:30:23 +0600

    First Lab

---
+++ 1.py
@@ -0,0 +1,4 @@
+a = [i for i in range(10) if i % 2 == 0]
+b = [i for i in range(10) if i % 2 != 0]
+z = a + b
+print(z)
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Web-Dev
+kotlinc 1.kt -include-runtime -d 1.jar
+java -jar 1.jar
