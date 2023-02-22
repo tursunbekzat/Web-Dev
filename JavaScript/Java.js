a56788fb2bdb9c5db3dc0f1e6ead43c293e0adbf
Bekzat <b_tursyn@kbtu.kz>
2023-02-22 22:09:35 +0600

    lab4

---
+++ JavaScript/Java.js
@@ -0,0 +1,15 @@
+const array = [1, 2, 3, 5, 8]
+console.log(array)
+array.push(13)
+console.log(array)
+
+
+function sum(a, b){
+    return a+b
+}
+const sum = (a, b) => {
+    return a + b
+}
+
+const cub = a => a ** 3
+
