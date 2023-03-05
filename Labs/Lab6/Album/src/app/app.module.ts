987c435171ba4bb67c9633141437e41c1c458dde
Bekzat <b_tursyn@kbtu.kz>
2023-03-05 17:34:19 +0600

    lab6

--- Labs/Lab6/Album/src/app/app.module.ts
+++ Labs/Lab6/Album/src/app/app.module.ts
@@ -1,11 +1,17 @@
-import { NgModule } from '@angular/core';
+import {Component, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from "@angular/forms";
+import { RouterModule, Routes } from "@angular/router";
 
-import { AppRoutingModule } from './app-routing.module';
+// import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarComponent } from './components/car/car.component';
 import { ContactsComponent } from './components/contacts/contacts.component';
+
+const appRoutes: Routes = [
+  {path:'', component:CarComponent},
+  {path:'about', component:ContactsComponent}
+]
 
 @NgModule({
   declarations: [
@@ -15,8 +21,9 @@ import { ContactsComponent } from './components/contacts/contacts.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    // AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
