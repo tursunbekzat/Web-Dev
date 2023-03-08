c453496916b01425ab2fd8fbf0e1930b3347cdda
Bekzat <b_tursyn@kbtu.kz>
2023-03-08 15:19:29 +0600

    pr

--- Practises/Web_Developer/Web_Developer/src/app/app.module.ts
+++ Practises/Web_Developer/Web_Developer/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DateComponent } from './components/date/date.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DateComponent
   ],
   imports: [
     BrowserModule,
