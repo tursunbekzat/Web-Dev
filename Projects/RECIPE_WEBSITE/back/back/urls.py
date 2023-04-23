a761e63f54cbf75030ba93ed126f172a596c26aa
Bekzat <b_tursyn@kbtu.kz>
2023-04-24 05:32:46 +0600

    aaaaaaaaa

--- Projects/RECIPE_WEBSITE/back/back/urls.py
+++ Projects/RECIPE_WEBSITE/back/back/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for back project.
+"""back URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,11 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-# from api import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api', include('api.urls')),
-    # path('api/recipe/<int:id>/vacancy/', views.ByCompany),
-    # path('api-auth/', include('rest_framework.urls'))
+    path('api/', include('api.urls')),
 ]
